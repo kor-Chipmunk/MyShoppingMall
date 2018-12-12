3f3a56f7edaec11a444f256f841d5a24b375aa14
Chipmunk <rhj4862@gmail.com>
2018-12-13 01:01:32 +0900

    Add Cart System

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create! id: 1, name: "T-Shirt", price: 12.49, active: true
+Product.create! id: 2, name: "Hoodie", price: 16.49, active: true
+Product.create! id: 3, name: "Blue jean", price: 10.29, active: true
+
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
